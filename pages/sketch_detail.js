1f31a47234bfe0ce6981ddf9afa629b7c4552642
matildecurino <matilde.curino0509@gmail.com>
2026-01-29 17:54:14 +0100

    . non cambia le modifiche uffa

--- pages/sketch_detail.js
+++ pages/sketch_detail.js
@@ -1280,7 +1280,7 @@ function drawYearNavigator(year) {
   let inactiveColor = color(180);
   let arrowColor = hasMultipleEruptions ? activeColor : inactiveColor;
 
-  let margin = 61;
+  let margin = 62;
   let y = 230;
   let navigatorX = margin;
 
